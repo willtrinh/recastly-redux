2d54b5b531ef113ae9a567fa805f6c758e15d230
Will Trinh <williamltrinh@gmail.com>
2021-03-25 15:58:35 -0700

    Updated search action to debounce ajax requests to 500ms & passed handle search in index.js

--- src/actions/search.js
+++ src/actions/search.js
@@ -2,18 +2,15 @@ import searchYouTube from '../lib/searchYouTube.js';
 import changeVideoList from './videoList.js';
 import changeVideo from './currentVideo.js';
 import YOUTUBE_API_KEY from '../config/youtube.js';
-
+import _ from 'lodash';
 
 var handleVideoSearch = q => {
-  return dispatch => {
-    setTimeout(() => {
-      return searchYouTube({key: YOUTUBE_API_KEY, query: q, max: 5}, videos => {
-        dispatch(changeVideo(videos[0]));
-        dispatch(changeVideoList(videos));
-      });
-    }, 500);
-
-  };
+  return _.debounce((dispatch) => {
+    searchYouTube({key: YOUTUBE_API_KEY, query: q, max: 5}, videos => {
+      dispatch(changeVideo(videos[0]));
+      dispatch(changeVideoList(videos));
+    });
+  }, 500);
 };
 
 export default handleVideoSearch;
--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@ import searchYouTube from './lib/searchYouTube.js';
 import YOUTUBE_API_KEY from './config/youtube.js';
 import store from './store/store.js';
 import { Provider } from 'react-redux';
+import handleSearchChange from './actions/search.js';
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('app')
+  document.getElementById('app'),
+  () => handleSearchChange('shiba inu')(store.dispatch)
 );
