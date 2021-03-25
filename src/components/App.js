544aeb8bbdb5bf179cbc777ac2c166a586228d35
Will Trinh <williamltrinh@gmail.com>
2021-03-24 17:27:29 -0700

    Implemented Store & Refactor App Component

--- src/components/App.js
+++ src/components/App.js
@@ -2,40 +2,49 @@ import React from 'react';
 import VideoList from './VideoList.js';
 import VideoPlayer from './VideoPlayer.js';
 import Search from './Search.js';
+import changeVideo from '../actions/currentVideo.js';
+import changeVideoList from '../actions/videoList.js';
+import store from '../store/store.js';
+
+import VideoListContainer from '../containers/VideoListContainer.js';
+import VideoPlayerContainer from '../containers/VideoPlayerContainer.js';
+import SearchContainer from '../containers/SearchContainer.js';
+
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-      videos: [],
-      currentVideo: null
-    };
-
-    this.getYouTubeVideos = this.getYouTubeVideos.bind(this);
   }
 
-  componentDidMount() {
-    this.getYouTubeVideos('react tutorials');
-  }
+  //   this.state = {
+  //     videos: [],
+  //     currentVideo: null
+  //   };
 
-  handleVideoListEntryTitleClick(video) {
-    this.setState({currentVideo: video});
-  }
+  //   this.getYouTubeVideos = this.getYouTubeVideos.bind(this);
+  // }
 
-  getYouTubeVideos(query) {
-    var options = {
-      key: this.props.API_KEY,
-      query: query
-    };
+  // componentDidMount() {
+  //   this.getYouTubeVideos('react tutorials');
+  // }
 
-    this.props.searchYouTube(options, (videos) =>
-      this.setState({
-        videos: videos,
-        currentVideo: videos[0]
-      })
-    );
-  }
+  // handleVideoListEntryTitleClick(video) {
+  //   this.setState({currentVideo: video});
+  // }
+
+  // getYouTubeVideos(query) {
+  //   var options = {
+  //     key: this.props.API_KEY,
+  //     query: query
+  //   };
+
+  //   this.props.searchYouTube(options, (videos) =>
+  //     this.setState({
+  //       videos: videos,
+  //       currentVideo: videos[0]
+  //     })
+  //   );
+  // }
 
   //TODO: swap out the React components below for the container components
   //  you wrote in the 'containers' directory.
@@ -44,18 +53,15 @@ class App extends React.Component {
       <div>
         <nav className="navbar">
           <div className="col-md-6 col-md-offset-3">
-            <Search getYouTubeVideos={this.getYouTubeVideos}/>
+            <SearchContainer />
           </div>
         </nav>
         <div className="row">
           <div className="col-md-7">
-            <VideoPlayer video={this.state.currentVideo}/>
+            <VideoPlayerContainer />
           </div>
           <div className="col-md-5">
-            <VideoList
-              handleVideoListEntryTitleClick={this.handleVideoListEntryTitleClick.bind(this)}
-              videos={this.state.videos}
-            />
+            <VideoListContainer />
           </div>
         </div>
       </div>
@@ -63,4 +69,29 @@ class App extends React.Component {
   }
 }
 
+// render() {
+//   return (
+//     <div>
+//       <nav className="navbar">
+//         <div className="col-md-6 col-md-offset-3">
+//           <SearchContainer getYouTubeVideos={this.getYouTubeVideos}/>
+//         </div>
+//       </nav>
+//       <div className="row">
+//         <div className="col-md-7">
+//           <VideoPlayerContainer video={this.state.currentVideo}/>
+//         </div>
+//         <div className="col-md-5">
+//           <VideoList
+//             handleVideoListEntryTitleClick={this.handleVideoListEntryTitleClick.bind(this)}
+//             videos={this.state.videos}
+//           />
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
 export default App;
+
+
--- src/store/store.js
+++ src/store/store.js
@@ -4,3 +4,11 @@ import rootReducer from './../reducers/main.js';
 import exampleVideoData from '../data/exampleVideoData.js';
 
 // TODO:  Create your redux store, apply thunk as a middleware, and export it!
+
+var configureStore = createStore(
+  rootReducer, {currentVideo: exampleVideoData[0]},
+  applyMiddleware(thunk)
+);
+
+export default configureStore;
+
