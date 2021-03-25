35190856b891ef0b88d1cba008826eef3c8f2dfa
Will Trinh <williamltrinh@gmail.com>
2021-03-25 10:07:32 -0700

    refactor code

--- src/reducers/currentVideo.js
+++ src/reducers/currentVideo.js
@@ -4,7 +4,7 @@ var currentVideoReducer = (state = null, action) => {
   //TODO: define a reducer for the currentVideo field of our state.
   switch (action.type) {
   case 'CHANGE_VIDEO':
-    return action.video;
+    return action.video || null;
   default:
     return state;
   }
